b2a5738a1fae1fbc138fc70e81dd1cee3aa18542
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-19 19:43:25 -0400

    Get new user form functionality

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
 
-# @app.get("/")
-# def
+@app.get("/")
+def display_home_page():
+    """Presents the home page to the user"""
+
+    #for now, redirect to users page
+
+    return redirect("/users")
 
 
 # @app.post("/")
@@ -25,7 +30,48 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/users")
 def display_users():
+    """Display the users page"""
+
     users = User.query.all()
-    return render_template("users.html",
-                     title="Users",
-                     users=users)
+    return render_template(
+        "users.html",
+        title="Users",
+        users=users
+    )
+
+@app.get('/users/new')
+def display_new_user():
+    """Displays the form for a new user"""
+
+    return render_template(
+        "new_user.html",
+        title='Create New User'
+
+    )
+
+@app.post('/users/new')
+def create_new_user():
+    """Handles form submission for a new user"""
+
+    print('YOU GOT HERE ------')
+
+    data = request.form
+    print(data, 'this is data')
+
+    first_name = data.get('first-name')
+    last_name = data.get('last-name')
+    image_url = data.get('image-url')
+
+    image_url = image_url if image_url else None
+
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url
+    )
+
+    db.session.add(new_user)
+
+    db.session.commit()
+
+    return redirect('/')
